4418cf05066a2368b6b737f2fe0b9f6384e806be
nandab-Isengard <nandab-Isengard>
2026-06-29 22:04:50 +0000

    Test

---
+++ jobs/TestFirstJob/TestFirstJob.py
@@ -0,0 +1,15 @@
+import sys
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+
+sc = SparkContext.getOrCreate()
+spark = SparkSession.builder.getOrCreate()
+
+# Script generated for node S3DataSource
+S3DataSource_1782769848785 = spark.read.format("csv") \
+    .option("inferschema", "true") \
+    .option("multiLine", "true") \
+    .option("header", "true") \
+    .option("recursiveFileLookup", "true") \
+    .option("sep", ",") \
+    .load("s3://amazon-sagemaker-211125551132-us-east-2-cc5nijmuehwkyx/shared/")
